575d148a25de0e4fe5ce9b9c288a69ca3a56c8ee
Artemiy <artemiy.moroz@gmail.com>
2025-11-21 17:43:06 +0200

    Lab 4 completed + documentation

--- src/main/java/org/example/springlab2/controller/TaskController.java
+++ src/main/java/org/example/springlab2/controller/TaskController.java
@@ -1,18 +1,25 @@
 package org.example.springlab2.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.example.springlab2.dto.inbound.TaskDtoInbound;
+import org.example.springlab2.dto.outbound.TaskDtoOutbound;
+import org.example.springlab2.entity.Task;
 import org.example.springlab2.service.TaskService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
-@Controller
-@RequestMapping("/tasks")
+@RestController
+@RequestMapping("/api/v1/tasks")
 @Validated
 public class TaskController {
 
@@ -22,48 +29,55 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @Operation(summary = "Get all tasks",
+            description = "Returns list of all tasks. Can sort by priority or date")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully got a list"),
+    })
     @GetMapping
-    public String getAll(
-            @RequestParam(name = "sort", required = false) String sort,
-            Model model) {
-        if ("priority".equals(sort)) {
-            model.addAttribute("tasks", taskService.sortByPriority());
-        } else if ("date".equals(sort)) {
-            model.addAttribute("tasks", taskService.sortByDate());
-        } else {
-            model.addAttribute("tasks", taskService.findAll());
-        }
-        return "index.html";
+    public List<TaskDtoOutbound> getAll(
+            @RequestParam(required = false) String sort
+            ) {
+        return switch (sort){
+            case "priority" -> taskService.sortByPriority();
+            case "date" -> taskService.sortByDate();
+            default -> taskService.findAll();
+        };
     }
 
+    @Operation(summary = "Create task",
+            description = "Create task with provided JSON")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully created task"),
+            @ApiResponse(responseCode = "400", description = "Validation failed")
+    })
     @PostMapping
-    public String createTask(@Valid TaskDtoInbound inbound) {
-        taskService.addTask(inbound);
-        return "redirect:/tasks";
+    public ResponseEntity<Task> createTask(@Valid @RequestBody TaskDtoInbound inbound) {
+        Task new_task = taskService.addTask(inbound);
+        return ResponseEntity.status(201).body(new_task);
     }
 
-    @PostMapping("/complete/{id}")
-    public String toggleComplete(@PathVariable("id") UUID id) {
+    @Operation(summary = "Mark task as complete",
+    description = "Marks task by id as completed")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Task marked as completed"),
+            @ApiResponse(responseCode = "404", description = "Task not found")
+    })
+    @PatchMapping("/{id}/complete")
+    public ResponseEntity<Task> toggleComplete(@PathVariable("id") UUID id) {
         taskService.toggleComplete(id);
-        return "redirect:/tasks";
+        return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Delete task",
+    description = "Delete task by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successfully deleted task"),
+            @ApiResponse(responseCode = "404", description = "Task not found")
+    })
     @DeleteMapping("/{id}")
-    public String deleteTask(@PathVariable("id") UUID id) {
+    public ResponseEntity<Task> deleteTask(@PathVariable("id") UUID id) {
         taskService.removeById(id);
-        return "redirect:/tasks";
+        return ResponseEntity.noContent().build();
     }
-
-    @PostMapping("/priority")
-    public String sortByPriority(Model model){
-        model.addAttribute("tasks", taskService.sortByPriority());
-        return "index.html";
-    }
-
-    @PostMapping("/date")
-    public String sortByDate(Model model){
-        model.addAttribute("tasks", taskService.sortByDate());
-        return "index.html";
-    }
-    
 }
